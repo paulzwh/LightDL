de7b9a7b5d290cda7c496d98c834d7a31d85c2d2
paulzwh <51662273+paulzwh@users.noreply.github.com>
2024-01-24 20:15:08 +0800

    add count num params

--- pytorch/utils/misc.py
+++ pytorch/utils/misc.py
@@ -3,6 +3,9 @@ from argparse import Namespace
 from pathlib import Path
 import json
 
+import logging
+import torch
+
 
 class ExtendedJSONEncoder(json.JSONEncoder):
     def default(self, o: Any) -> Any:
@@ -17,3 +20,13 @@ def save_args(args: Namespace):
     
     with open(args.output / f"args_node{args.node_rank}.json", "w") as fp:
         json.dump(vars(args), fp, indent=4, cls=ExtendedJSONEncoder)
+
+
+def count_num_params(model: torch.nn.Module, logger: logging.Logger = None):
+    train_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    total_params = sum(p.numel() for p in model.parameters())
+    
+    if logger is not None:
+        logger.info(f"# total params: {total_params}, # train params: {train_params}.")
+
+    return total_params, train_params
