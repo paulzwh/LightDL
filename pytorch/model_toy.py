9d9f4f92be942036249a1416ae76218b1623acb6
paulzwh <51662273+paulzwh@users.noreply.github.com>
2024-01-26 16:18:30 +0800

    update model_toy

--- pytorch/model_toy.py
+++ pytorch/model_toy.py
@@ -4,11 +4,13 @@ from torch import nn
 class ToyNet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, padding=1)
-        self.fc = nn.Linear(in_features=28 * 28, out_features=2)
-        self.flatten = nn.Flatten()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
+            nn.Linear(in_features=14 * 14, out_features=2)
+        )
     
     def forward(self, x):
-        x = self.conv(x)
-        x = self.flatten(x)
-        return self.fc(x)
+        return self.conv(x)
